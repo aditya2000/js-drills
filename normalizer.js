9903217cf6b056fdaf4724f1efd81e8ec6403b99
bw99214@gmail.com <bw99214@gmailcom>
2018-03-15 23:54:38 +0530

    Initial commit

---
+++ normalizer.js
@@ -0,0 +1,21 @@
+function textNormalizer(text) {
+  return text.toLowerCase().trim();
+}
+
+
+
+
+// tests
+
+function testTextNormalizer() {
+  var text = "   let's GO SURFING NOW everyone is learning how   ";
+  var expected = "let's go surfing now everyone is learning how";
+  if (textNormalizer(text) === expected) {
+    console.log('SUCCESS: `textNormalizer` is working');
+  }
+  else {
+    console.log('FAILURE: `textNormalizer` is not working');
+  }
+}
+
+testTextNormalizer();
